b75fa04696f6d662d81b3bf8640760afc90db1d5
Marek Rogiński <marekroginski1994@gmail.com>
2023-06-21 23:52:01 +0200

    the slider is almost ready

---
+++ alpacas/src/components/data.js
@@ -0,0 +1,32 @@
+import zwierzę1 from "./public/zwierzę1.jpg";
+import zwierzę2 from './public/zwierzę2.jpg';
+import zwierzę3 from './public/zwierzę3.jpg';
+import zwierzę4 from './public/zwierzę4.jpg';
+import zwierzę5 from './public/zwierzę5.jpg';
+
+
+const slideItems = [
+  {
+    image: zwierzę1,
+    
+  },
+  {
+    image: zwierzę2,
+    
+  },
+  {
+    image: zwierzę3,
+    
+  },
+  {
+    image: zwierzę4,
+    
+  },
+  {
+    image: zwierzę5,
+    
+  },
+ 
+];
+
+export default slideItems;
