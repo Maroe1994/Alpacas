9d9adff25f418f66198b96e88793df791ea4ddfb
Marek Rogiński <marekroginski1994@gmail.com>
2023-12-14 23:16:19 +0100

    Slide show ready

--- alpacas/src/components/Examplemodal.js
+++ alpacas/src/components/Examplemodal.js
@@ -3,7 +3,20 @@ import React from "react";
 import Modal from 'react-modal'
 import { useState,useEffect } from "react";
 
+
+
 const Galeries=["animala.jpg","animalb.jpg","animalc.jpg","animald.jpg","animale.jpg"]
+
+const customStyles = {
+    content: {
+      
+      background: "#52565c"
+      
+    },
+    img:{
+        border: "15px"
+    }
+  };
 
 export function Examplemodal() {
      
@@ -28,22 +41,29 @@ export function Examplemodal() {
       }, []);
     function nextimage () {
         setimage(image+1) 
-        
-
-        
-    }
+       
+        if (image>= 4){
+           setimage(0) 
+        }
+    
+        }
     function previousimage (){
         setimage(image-1)
         
+        if (image<= 0){
+            setimage(4)
+        }
     }
     return(
         <div>
             <button onClick={openModal}>open modal</button>
-    <Modal width={1400} height={665} isOpen={modalopen} >
+    <Modal width={1400} height={690} isOpen={modalopen} style={customStyles} >
+        
         <button  onClick={nextimage}>+</button>
         <button onClick={previousimage}>-</button>
-<img width={1380} height={615} src={Galeries[image]}  alt=""/>
-<button onClick={closeModal}>close modal</button>
+<img  width={1380} height={600} src={Galeries[image]}  alt=""/>
+<button onClick={closeModal}>Zamknij</button>
+
     </Modal>
     </div>
 )} ;
